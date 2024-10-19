4e3be2cde48df3d86c975285363fdda208d09188
Danny <hoffswelldanny@gmail.com>
2024-10-18 21:44:25 -0500

    dialogue trigger start

--- DisruptiveDog/Assets/Dialogue/DialogueTriggerBox.cs
+++ DisruptiveDog/Assets/Dialogue/DialogueTriggerBox.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class DialogueTriggerBox : MonoBehaviour
 {
+    public DialogueMessageBundle BundleToTrigger;
+    public DialogueTriggerBox[] PrerequisiteTriggers;
+
+    public bool AlreadyTriggered
+    {
+        get { return m_burned; }
+    }
+
+    private bool m_burned;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_burned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_burned)
+            return;
+
+        foreach (DialogueTriggerBox trigger in PrerequisiteTriggers)
+        {
+            if (!trigger.AlreadyTriggered)
+            {
+                Debug.Log("Prereq triggers not met");
+                return;
+            }
+        }
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        Debug.Log("Player Overlapt with " + gameObject.name);
+
+        DialogueFeed.Get().StartDialogue(BundleToTrigger);
+        m_burned = true;
     }
 }
